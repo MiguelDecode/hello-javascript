97ea8f5440babade5ac2d9a10df6707345894e66
migueldecode <migueldecode@gmail.com>
2024-07-11 23:00:49 +0200

    First JavaScript Lesson by Mouredev

---
+++ Basic/00-helloworld.js
@@ -0,0 +1,21 @@
+// Esto es un comentario
+
+/*
+    Comentario
+    de multiples
+    líneas
+*/
+
+console.log("Hola JavaScript!!");
+console.log("Hola JavaScript!!");
+console.log("Hola JavaScript!!");
+
+
+console.log(5)
+console.log("5")
+console.log(5 + 2)
+console.log(5 - 2)
+console.log(5 * 2)
+console.log(5 / 2)
+console.log(5 % 2)
+
